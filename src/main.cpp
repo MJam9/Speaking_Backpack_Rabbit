999f8e3e62e8db34992e637399ab7f02e4f38640
Marijam Schmidt <marijam.schmidt1@tha.de>
2026-06-03 17:42:37 +0200

    add light sleep mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
-#include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"//Download hier zu finden: https://wiki.dfrobot.com/DFPlayer_Mini_SKU_DFR0299
 #include <IRrecv.h>
 #include <IRremoteESP8266.h>
 #include <IRutils.h>
+#include <esp_sleep.h>
 
 // Servo-Objekt
 Servo servoRechts;
@@ -22,7 +22,7 @@ const int servolinksPin = 26;  // Signal-Pin für das Servo (GPIO 19 alt)
 //decode_results results;
 
 // Zeitsteuerung
-unsigned long interval = 5 * 1000; // 5 Sekunden in ms
+unsigned long interval = 10 * 1000; // 10 Sekunden in ms
 unsigned long lastActionTime = 0;
 
 // Bewegungs-Definitionen
@@ -69,10 +69,23 @@ const int anzahlBewegungen = sizeof(bewegungen) / sizeof(bewegungen[0]);
 
 void spieleZufallsTrack() {
   int trackNumber = random(1, 4);  // Track 1 bis 3
-  //Serial.print("Spiele Track Nummer: ");
-  //Serial.println(trackNumber);
+  Serial.print("Spiele Track Nummer: ");
+  Serial.println(trackNumber);
   myDFPlayer.play(trackNumber);
-  delay(5000);
+
+  // Wait until the track is finished (or up to 10 seconds max)
+  unsigned long waitStart = millis();
+  while (millis() - waitStart < 10000UL) {
+    if (myDFPlayer.available()) {
+      uint8_t type = myDFPlayer.readType();
+      if (type == DFPlayerPlayFinished) {
+        Serial.println("Track finished.");
+        break;
+      }
+      myDFPlayer.read();
+    }
+    delay(20);
+  }
 }
 
 void bewegeServosZufaellig() {
@@ -87,7 +100,7 @@ void bewegeServosZufaellig() {
     int rechts = b.sequenz[i][0];
     int links  = b.sequenz[i][1];
 
-    //Serial.printf("  -> Servo rechts = %d, links = %d\n", rechts, links);
+    Serial.printf("  -> Servo rechts = %d, links = %d\n", rechts, links);
 
     servoRechts.write(rechts);
     servoLinks.write(links);
@@ -109,16 +122,17 @@ void servoAn() {
 
 void setup() {
   Serial.begin(115200);
+  randomSeed(analogRead(0)); // Initialisiert den Zufallsgenerator
 
   mySerial.begin(9600, SERIAL_8N1, 16, 17);  // RX, TX
   delay(1000);
 
   if (!myDFPlayer.begin(mySerial)) {
-    //Serial.println("DFPlayer nicht verbunden. Schlafe endlos.");
+    Serial.println("DFPlayer nicht verbunden. Schlafe endlos.");
     while (true);  // Blockieren statt Sleep
   }
 
-  //Serial.println("DFPlayer erfolgreich verbunden.");
+  Serial.println("DFPlayer erfolgreich verbunden.");
   myDFPlayer.volume(30);  // Lautstärke
 
   servoRechts.attach(servorechtsPin);
@@ -129,14 +143,15 @@ void setup() {
 
 void loop() {
   if (millis() - lastActionTime >= interval) {
-    lastActionTime = millis();
+    unsigned long cycleStart = millis();
+    lastActionTime = cycleStart;
 
     // Zufallsmodus: 0 = Sprechen, 1 = Bewegen, 2 = Beides
     int mode = random(0, 3);
     // mode = 2;  // Zum Testen gezielt nur "beides"
 
-    //Serial.print("Modus gewählt: ");
-    //Serial.println(mode);
+    Serial.print("Modus gewählt: ");
+    Serial.println(mode);
 
     if (mode == 0 || mode == 2) {
       spieleZufallsTrack();
@@ -145,17 +160,25 @@ void loop() {
     if (mode == 1 || mode == 2) {
       //servoAn();
       bewegeServosZufaellig();
-      delay(500);  // kurz stabilisieren
+      //delay(500); // Kann meist entfernt werden, wenn die Sequenz eigene Delays hat
       //servoAus();
     }
 
-    // Warten vor Sleep, damit DFPlayer etc. fertig sind
-    delay(5000); 
+    unsigned long actionDuration = millis() - cycleStart;
+    unsigned long sleepTime = 0;
 
-    /*// Light Sleep starten
-    Serial.println("Gehe in Light Sleep...");
-    esp_sleep_enable_timer_wakeup(interval * 1000);  // Mikrosekunden
-    esp_light_sleep_start();
-    Serial.println("Aufgewacht aus Light Sleep.");*/
+    if (actionDuration < interval) {
+      sleepTime = interval - actionDuration;
+    }
+
+    if (sleepTime > 0) {
+      Serial.printf("Action duration: %lu ms, entering light sleep for %lu ms\n", actionDuration, sleepTime);
+      esp_sleep_enable_timer_wakeup(sleepTime * 1000ULL);  // microseconds
+      esp_light_sleep_start();
+      Serial.println("Woke up from light sleep.");
+      lastActionTime = millis();
+    } else {
+      Serial.printf("Action took %lu ms, skipping sleep this cycle.\n", actionDuration);
+    }
   }
 }
